78634ad0a9974eea6c750b78f552bd7f7566a79e
Josh <neversitdull@icloud.com>
2024-11-24 07:25:41 -0800

    add umami analytics and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [partytown()],
 });
