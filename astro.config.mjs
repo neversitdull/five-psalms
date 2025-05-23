5fb481081d386f4f232419ab302a6d4132dada61
Josh Dunsterville <neversitdull@icloud.com>
2025-05-23 06:33:04 -0700

    Add new astro fonts api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,35 @@
 // @ts-nocheck
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config"
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Geist",
+        cssVariable: "--font-geist",
+        weights: ["100 900"],
+        styles: ["normal"],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Geist Mono",
+        cssVariable: "--font-geist-mono",
+        weights: ["100 900"],
+        styles: ["normal"],
+        subsets: ["latin"],
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://fivepsalms.com",
   integrations: [sitemap()],
-});
+})
